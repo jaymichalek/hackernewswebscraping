0b77db33cf3e1d28f3939b802661b713eec6dd01
hashtagd3v <jaycoinminer@gmail.com>
2022-03-14 14:21:57 -0500

    Used pprint module to print data neatly on console. Created function to enumerate in console the title, link and votes of articles with > 99 votes only.

--- scraping.py
+++ scraping.py
@@ -1,9 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
 # Grabs the links for the story title:
 links = soup.select('.titlelink')
 # Grabs all the votes for stories:
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+pprint.pprint(create_custom_hn(links, subtext))
