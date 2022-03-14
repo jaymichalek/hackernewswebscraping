7bf9266aba547912364f287d8128ecd60894ece0
hashtagd3v <jaycoinminer@gmail.com>
2022-03-14 14:54:43 -0500

    Scrape second page

--- scraping.py
+++ scraping.py
@@ -3,11 +3,19 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/')
+# Grabs page 2 of hacker news:
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 # Grabs the links for the story title:
 links = soup.select('.titlelink')
+links2 = soup2.select('.titlelink')
 # Grabs all the votes for stories:
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 
 # Function sorts the hashmap from create_custom_hn fn to a list in reverse order
@@ -32,4 +40,4 @@ def create_custom_hn(link, text):
 
 
 # Used pprint to print output neatly on console.
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
